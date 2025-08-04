9b083fd432ea7c2960bf022e37e678dad834cfb7
compost-em-all <jtaylor9110@gmail.com>
2025-08-03 21:21:48 -0400

    wip - getting data into table

--- CustomerMatterManagerUI/src/App.tsx
+++ CustomerMatterManagerUI/src/App.tsx
@@ -1,10 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DataTable from './components/dataTable';
+import type { ColumnDef } from '@tanstack/react-table';
+import type { CustomerDTO } from './models/customerDto';
+
+const columns: ColumnDef<CustomerDTO>[] = [
+  {
+    accessorKey: 'customerId',
+    header: 'ID',
+    cell: info => info.getValue(),
+  },
+  {
+    accessorKey: 'customerName',
+    header: 'Name',
+    cell: info => info.getValue(),
+  },
+  {
+    accessorKey: 'phoneNum',
+    header: 'Phone Number',
+    cell: info => info.getValue(),
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState<CustomerDTO[]>([]);
+
+  const handleRowAction = (row: CustomerDTO) => {
+    alert(`Open modal for ${row.name}`);
+  };
+
+  // get data from API
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(`${import.meta.env.VITE_API_URL}`);
+
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/customer`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('Response status:', response);
+
+    };
+
+    fetchData();
+
+    // fetch(`${import.meta.env.VITE_API_URL}/customers`)
+    //   .then(response => response.json())
+    //   .then(data => setData(data))
+    //   .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <>
@@ -31,6 +81,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="mt-8">
+        <DataTable data={data} columns={columns} onRowAction={handleRowAction} />
+      </div>
     </>
   )
 }
